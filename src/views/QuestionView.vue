a0efa4513d119a2f19658797942dee8b1182ef13
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-09 14:07:58 +0200

    add start view and question view

---
+++ src/views/QuestionView.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import ButtonComponent from "../components/ButtonComponent.vue";
+
+export default {
+  props: {
+    question: String,
+    answers: [],
+  },
+  methods: {
+    consoleClick() {
+      console.log("answer was choosen");
+    },
+  },
+  components: {
+    ButtonComponent,
+  },
+};
+</script>
+
+<template>
+  <h1>Question</h1>
+  <p>{{ question }}</p>
+  <div v-for="(answer, index) in answers" :key="index">
+    <ButtonComponent :value="answer" :onClick="consoleClick" />
+  </div>
+</template>
---
+++ src/views/StartView.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import ButtonComponent from "../components/ButtonComponent.vue";
+import { useQuizStore } from "@/stores/quizStore";
+import { mapActions } from "pinia";
+
+export default {
+  data() {
+    return {
+      btnValue: "Start the quiz!",
+    };
+  },
+  methods: {
+    ...mapActions(useQuizStore, ["fetchQuestions"]),
+    startQuiz() {
+      this.fetchQuestions();
+      console.log("start quiz");
+    },
+  },
+  components: { ButtonComponent },
+};
+</script>
+
+<template>
+  <h1>Welcome to Trivia App</h1>
+  <ButtonComponent :value="btnValue" :onClick="startQuiz" />
+</template>
