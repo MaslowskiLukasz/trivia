a9594d3cf3d6eea9211c7c7463e6fba5ba17ee1b
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-09 14:07:10 +0200

    create quiz store

---
+++ src/stores/quizStore.ts
@@ -0,0 +1,45 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export interface Question {
+  question: string;
+  answers: string[];
+  correctAnswer: number;
+  selectedAnswer?: number;
+}
+
+export const useQuizStore = defineStore({
+  id: "quiz",
+  state: () => ({
+    questions: [] as Question[],
+    currentQuestion: 0,
+    questionsLoaded: false,
+  }),
+  actions: {
+    async fetchQuestions() {
+      const url = "https://opentdb.com/api.php?amount=10&type=multiple";
+      const response = await axios.get(url);
+      const results = await response.data.results;
+      const data: Question[] = [];
+
+      results.map((item) => {
+        data.push({
+          question: item.question,
+          answers: [...item.incorrect_answers, item.correct_answer],
+          correctAnswer: item.correct_answer,
+        });
+      });
+
+      this.questions = [...data];
+      this.questionsLoaded = true;
+      console.log(`store = ${JSON.stringify(this.questions)}`);
+      console.log(`question loaded = ${this.questionsLoaded}`);
+    },
+    gotoNextQuestion() {
+      this.currentQuestion++;
+    },
+    gotoPreviousQuestion() {
+      this.currentQuestion--;
+    },
+  },
+});
