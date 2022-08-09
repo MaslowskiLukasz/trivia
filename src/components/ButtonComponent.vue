961fbcb567be86148fb1b793c257d40ab8087b61
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-09 14:08:14 +0200

    create button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  props: {
+    value: String,
+    onClick: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="onClick">{{ value }}</button>
+</template>
