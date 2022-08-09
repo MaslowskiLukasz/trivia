583b25670fc5b2e20996ef7ea036111c0a05137f
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-09 14:09:42 +0200

    create prototype with fetching, displaying quiz questions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import StartView from "./views/StartView.vue";
+import QuestionView from "./views/QuestionView.vue";
+import { mapStores } from "pinia";
+import { useQuizStore } from "./stores/quizStore";
+import ButtonComponent from "./components/ButtonComponent.vue";
+
+export default {
+  data() {
+    return {
+      next: "Next",
+      previous: "Previous",
+    };
+  },
+  computed: {
+    ...mapStores(useQuizStore),
+  },
+  components: {
+    StartView,
+    QuestionView,
+    ButtonComponent,
+  },
+};
+</script>
 
 <template>
   <main>
-    <h1>Test</h1>
+    <StartView />
+    <p v-if="quizStore.questionsLoaded">
+      <QuestionView
+        :question="quizStore.questions[quizStore.currentQuestion].question"
+        :answers="quizStore.questions[quizStore.currentQuestion].answers"
+      />
+      <ButtonComponent
+        :value="previous"
+        :onClick="quizStore.gotoPreviousQuestion"
+      />
+      <ButtonComponent
+        :value="next"
+        :onClick="quizStore.gotoNextQuestion"
+      />
+    </p>
   </main>
 </template>
