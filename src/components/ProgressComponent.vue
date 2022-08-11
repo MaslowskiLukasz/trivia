2b6db6210330431bf00dcc179e88bfc02acf03c7
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-11 21:10:54 +0200

    add round button component for question navigation

---
+++ src/components/ProgressComponent.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+export default {
+  props: {
+    number: Number,
+    onClick: Function,
+  },
+};
+</script>
+
+<template>
+  <button @click="onClick(number)">{{ number }}</button>
+</template>
+
+<style scoped>
+button {
+  display: block;
+  border: none;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  text-align: center;
+}
+</style>
