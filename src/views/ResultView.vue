a6b744ceffd02112fc5c6d6f945df9d4c4289887
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-11 02:56:32 +0200

    add ResultView

---
+++ src/views/ResultView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { useQuizStore } from "@/stores/quizStore";
+import { mapStores } from "pinia";
+
+export default {
+  computed: {
+    ...mapStores(useQuizStore),
+  },
+};
+</script>
+
+<template>
+  <h1>Results</h1>
+  <p>{{ quizStore.getResults() }} / {{ quizStore.questions.length }}</p>
+</template>
