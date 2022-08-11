3ce5c3f2a7b009f64a3dc72d1353d6d5865a5b52
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-11 21:13:01 +0200

    add question navigation; add layout styles

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -2,11 +2,15 @@
 import { useQuizStore } from "@/stores/quizStore";
 import { mapStores } from "pinia";
 import ButtonComponent from "../components/ButtonComponent.vue";
+import ProgressComponent from "../components/ProgressComponent.vue";
 
 export default {
   methods: {
     selectAnswer(index: number) {
       this.quizStore.selectAnswer(this.quizStore.currentQuestion, index);
+    },
+    changeQuestion(index: number) {
+      this.quizStore.gotoQuestion(index - 1);
     },
   },
   computed: {
@@ -14,38 +18,94 @@ export default {
   },
   components: {
     ButtonComponent,
+    ProgressComponent,
   },
 };
 </script>
 
 <template>
-  <h1>
-    Question nr {{ quizStore.currentQuestion + 1 }} /
-    {{ quizStore.questions.length }}
-  </h1>
-  <p>{{ quizStore.questions[quizStore.currentQuestion].question }}</p>
-  <div
-    v-for="(answer, index) in quizStore.questions[quizStore.currentQuestion]
-      .answers"
-    :key="index"
-  >
+  <section id="progress">
+    <div
+      class="question-number"
+      v-for="(question, index) in quizStore.questions"
+      :key="index"
+    >
+      <ProgressComponent
+        :number="index + 1"
+        :onClick="changeQuestion"
+        :class="[
+          typeof question.selectedAnswer !== 'undefined' ? 'selected' : '',
+        ]"
+      ></ProgressComponent>
+    </div>
+  </section>
+  <section id="question">
+    <h1>
+      Question nr {{ quizStore.currentQuestion + 1 }} /
+      {{ quizStore.questions.length }}
+    </h1>
+    <p>{{ quizStore.questions[quizStore.currentQuestion].question }}</p>
+  </section>
+  <section id="answers">
+    <div
+      class="answer"
+      v-for="(answer, index) in quizStore.questions[quizStore.currentQuestion]
+        .answers"
+      :key="index"
+    >
+      <ButtonComponent
+        :value="answer"
+        :onClick="selectAnswer"
+        :index="index"
+        :class="[
+          quizStore.questions[quizStore.currentQuestion].selectedAnswer === index
+            ? 'selected'
+            : '',
+        ]"
+      ></ButtonComponent>
+    </div>
+  </section>
+  <section id="navigation">
     <ButtonComponent
-      :value="answer"
-      :onClick="selectAnswer"
-      :index="index"
-      :class="[
-        quizStore.questions[quizStore.currentQuestion].selectedAnswer === index
-          ? 'selected'
-          : '',
-      ]"
+      :value="'Previous'"
+      :onClick="quizStore.gotoPreviousQuestion"
     />
-  </div>
-  <ButtonComponent
-    :value="'Previous'"
-    :onClick="quizStore.gotoPreviousQuestion"
-  />
-  <ButtonComponent
-    :value="'Next'"
-    :onClick="quizStore.gotoNextQuestion"
-  />
+    <ButtonComponent
+      :value="'Next'"
+      :onClick="quizStore.gotoNextQuestion"
+    />
+  </section>
 </template>
+
+<style>
+#question {
+  margin-bottom: 2em;
+}
+
+#answers {
+  padding: 2rem;
+  margin-bottom: 2em;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 1em;
+  grid-row-gap: 1em;
+}
+
+#progress {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin-bottom: 2rem;
+}
+
+.question-number {
+  margin: 0.2em;
+}
+
+#navigation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 10rem;
+}
+</style>
