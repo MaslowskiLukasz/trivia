3b2fb5294504587867bd6cba184d608586378d17
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-08-12 02:04:46 +0200

    add styles to start screen

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -21,6 +21,43 @@ export default {
 </script>
 
 <template>
-  <h1>Welcome to Trivia App</h1>
-  <ButtonComponent :value="btnValue" :onClick="startQuiz" />
+  <div id="wrapper">
+    <h1>Welcome to Trivia App!</h1>
+    <h2>Rules:</h2>
+    <ul>
+      <li>There is always only one correct answer</li>
+      <li>You can skip questions and come back to them later</li>
+      <li>You can change your answer until you hit submit</li>
+    </ul>
+    <ButtonComponent
+      class="submit-btn"
+      :value="btnValue"
+      :onClick="startQuiz"
+    />
+  </div>
 </template>
+
+<style scoped>
+#wrapper {
+  margin: 0 auto;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+}
+
+button {
+  margin-top: 3rem;
+  width: 80%;
+}
+
+h2 {
+  margin-top: 3rem;
+}
+
+@media (min-width: 550px) {
+  button {
+    width: 30%;
+  }
+}
+</style>
